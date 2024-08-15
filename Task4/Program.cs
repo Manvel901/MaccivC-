9f8dbaa8b7224aefd1f75650b98620b55c73ba0f
Manvel <Manvelnavoan@gmail.com>
2024-08-15 15:47:20 +0300

    First commit Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿//int n = 10;
+int[] arr = {3,4,1055,6,88,99,39,34,11,9};
+//int j = 0;
+int max = arr[0];
+foreach (int e in arr)
+{
+    if (e > max)
+    {
+         max = e;
+    }
+   
+}
+Console.Write(max);    
