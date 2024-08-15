29a6eb9f8f0fef43d4c70c520591523a22eaac58
Manvel <Manvelnavoan@gmail.com>
2024-08-15 14:53:55 +0300

    Firs commit Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = 15;
+int[] array = new int [n];
+int sum = 0;
+int j = 0;
+while (j<n)
+{
+    if (j % 2==0)
+    {
+        sum += j;
+    }
+     j++;
+}
+Console.Write(sum);
+Console.Write(" ");
