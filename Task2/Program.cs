1558089a3082cdc3e3792dbc82dc4bb4bbdc92f6
Manvel <Manvelnavoan@gmail.com>
2024-08-15 15:08:43 +0300

    first commit Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 4;
+int[] arr = {6, 5, 56, 33};
+int j = 0;
+int sum = 0;
+while (j<n)
+{
+    if (arr[j]%2==0)
+    {
+        sum += arr[j];
+    }
+    j++;
+}
+Console.Write($"{sum} ");
